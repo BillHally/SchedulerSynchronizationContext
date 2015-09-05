b8bfbbf56907e2a89a0fbe341a942e4b2256cb22
Bill Hally <bill.hally@gmail.com>
2015-09-05 11:04:49 +0100

    Initial version

---
+++ SchedulerSynchronizationContext.Tests/TestSchedulerSynchronizationContext.fs
@@ -0,0 +1,30 @@
+﻿module SchedulerSynchronizationContext.Tests
+
+open FsUnit
+open NUnit.Framework
+open SchedulerSynchronizationContext
+open Microsoft.Reactive.Testing
+
+[<Test>]
+let ``Async.SwitchToScheduler when invoked runs the continuation using the scheduler``() =
+    // Arrange
+    let s = TestScheduler()
+
+    let mutable state = 0
+
+    async
+        {
+            state <- 1
+            do! Async.SwitchToScheduler s
+            state <- 2
+        }
+        |> Async.StartImmediate
+
+    // Act
+    let before = state
+    s.Start()
+    let after = state
+
+    // Assert
+    before |> should equal 1
+    after  |> should equal 2
---
+++ SchedulerSynchronizationContext/SchedulerSynchronizationContext.fs
@@ -0,0 +1,31 @@
+﻿namespace SchedulerSynchronizationContext
+
+open System
+open System.Reactive
+open System.Reactive.Concurrency
+open System.Threading
+
+type SchedulerSynchronizationContext(s : IScheduler) =
+    inherit SynchronizationContext()
+
+    override this.Send(d : SendOrPostCallback, state : obj) =
+        use event = new AutoResetEvent(false)
+        
+        fun () ->
+            d.Invoke state |> ignore
+            event.Set()    |> ignore
+        |> s.Schedule
+        |> ignore
+
+        event.WaitOne() |> ignore
+
+    override this.Post(d : SendOrPostCallback, state : obj) =
+        fun () ->
+            d.Invoke state |> ignore
+        |> s.Schedule
+        |> ignore
+
+[<AutoOpen>]
+module AsyncExtensions =
+    type Microsoft.FSharp.Control.Async with
+        static member SwitchToScheduler (s : IScheduler) : Async<unit> = Async.SwitchToContext (SchedulerSynchronizationContext(s))
